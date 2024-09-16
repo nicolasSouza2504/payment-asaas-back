4c0ae1a1bda2da17b80164cdb8ef947af25ba5fd
nicolas <nicolas.souza@moinformatica.com>
2024-09-15 22:58:25 -0300

    Adjusts

--- src/main/java/org/dto/payment/PaymentExibitionDto.java
+++ src/main/java/org/dto/payment/PaymentExibitionDto.java
@@ -1,10 +1,12 @@
 package org.dto.payment;
 
+import java.util.Date;
+
 public class PaymentExibitionDto {
 
     private String paymentId;
     private String status;
     private String value;
     private String billingType;
-
+    private Date dateCreated;
 }
